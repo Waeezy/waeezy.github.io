ecb31a6c3cca9965abf71a3ab0c6ab2c8d46a106
Waeezy <41456031+Waeezy@users.noreply.github.com>
2020-04-28 15:47:53 -0700

    fix for clickable links

--- functions.js
+++ functions.js
@@ -13,3 +13,15 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+function openlink(num) {
+  if (num == 1){
+    window.open('https://www.facebook.com/waeely/', '_blank');
+  }
+  if (num == 2){
+    window.open('https://github.com/Waeezy', '_blank');
+  }
+  if (num == 3){
+    window.open('https://www.linkedin.com/in/wael-yakoub-agha-12a426158/', '_blank');
+  }
+}
